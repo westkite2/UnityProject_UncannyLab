9de4577ac00a95d69e218ff25bdc95db861bc5cc
jsh <bearang31@naver.com>
2022-05-29 15:49:36 +0900

    아이템 경비원

--- Assets/Script/jsh/playerAttak.cs
+++ Assets/Script/jsh/playerAttak.cs
@@ -6,6 +6,8 @@ public class playerAttak : MonoBehaviour
 {
     public GameObject bullet;
     public Transform pos;
+    public bool hasgun;
+    public bool hasbullet;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +17,27 @@ public class playerAttak : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.L))
-        {
-            Instantiate(bullet,pos.position, transform.rotation);
+    {   
+        if(hasbullet == true && hasgun == true)
+        {   if(Input.GetKeyDown(KeyCode.L))
+            {
+                Instantiate(bullet,pos.position, transform.rotation);
+            }
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "gun")
+        {
+            hasgun = true;
+        }
+
+        if(other.gameObject.tag == "bulletimage")
+        {
+            hasbullet = true;
+        }
+        
+    }
+
 }
